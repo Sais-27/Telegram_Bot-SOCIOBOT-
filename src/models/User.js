ff3005cb51472319e692f0d10bde2ca5927f8f29
Sai Sankatmochan <ssai_be21@thapar.edu>
2024-07-04 20:54:42 +0530

    Add files via upload

---
+++ src/models/User.js
@@ -0,0 +1,38 @@
+// src/models/User.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    tgId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    isBot: {
+        type: Boolean,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    promptTokens: {
+        type: Number,
+        required: false,
+    },
+    completionTokens: {
+        type: Number,
+        required: false,
+    },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+export default User;
